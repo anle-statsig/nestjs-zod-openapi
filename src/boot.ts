1f7ac4115a60830ce2e69569c574fec64c70c807
An Le <anle@statsig.com>
2024-08-15 13:05:34 -0700

    up

--- src/boot.ts
+++ src/boot.ts
@@ -4,4 +4,8 @@ import { z } from 'zod'
 extendZodWithOpenApi(z)
 
 // To export the global override of Zod types
-export { ZodOpenAPIMetadata } from '@asteasolutions/zod-to-openapi'
+export {
+    extendZodWithOpenApi,
+    ZodOpenAPIMetadata
+} from '@asteasolutions/zod-to-openapi'
+
